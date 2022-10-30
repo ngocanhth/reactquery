270bf9287a66dab242e4ce6a5d29972c69cd4335
ngocanh <lengocanhxd@gmail.com>
2022-10-30 08:49:24 +0700

    Bai 6 - KeepPreviousData

--- src/Post.jsx
+++ src/Post.jsx
@@ -7,18 +7,19 @@ import { useState } from 'react';
 
 function Post() {
   const [userId, setUserId] = useState(undefined);
+  const [page, setPage ] = useState(1);
 
     const fectApi = async (context) => {
     //  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
      console.log('context:', context)
-    const response = await axios.get('http://localhost:3001/posts');
+    const response = await axios.get(`http://localhost:3001/posts/${context.queryKey[1]}`);
       // console.log(response);
       return response.data;
     }
 
     const fectApi1 = async (context) => {
       console.log('context:', context)
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${context.queryKey[1]}`);
         return response.data;
     }
 
@@ -42,15 +43,18 @@ function Post() {
  // const { isError, isLoading, data, isFetching } = useQuery(['posts'], fectApi, {retry: 5, retryDelay: 1000, cacheTime: 10000})
  //  const { isError, isLoading, data } = useQuery(['posts'], fectApi, { staleTime: 4000 })
       
- const { isError, isLoading, isFetching, data } = useQuery(['posts', userId ], fectUserDetail, { enabled: !!userId })
-      console.log(data);
+ // const { isError, isLoading, isFetching, data } = useQuery(['posts', userId ], fectUserDetail, { enabled: !!userId })
+    //  console.log(data);
 
+    const { isError, isLoading, isFetching, isPreviousData, data } = useQuery(['posts', page], fectApi1, { keepPreviousData: true })
+
+      
       // console.log("Check loading: ", { isLoading, isFetching });
 
       // neu co loi co goi lai 1 lan moi lan 1s roi moi tra ve loi
       // retry: 5, retryDelay: 1000
 
-     if (isFetching) return <div> Loading React query...</div>
+      if (isLoading) return <div> Loading React query...</div>
 
       if (isError) return <div> error </div>
 
@@ -69,11 +73,17 @@ function Post() {
                 })
             }
           </ul> */}
+          
 
           <h3>React query - Depentdent </h3>
 
             <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder='userId'/>
           <h3>{data?.title}</h3>
+
+          Currentpage: { page }
+
+          <button onClick={() => setPage(prev => Math.max(1, (prev - 1)))} disabled = { page === 1 | isPreviousData}>Previous</button>
+          <button onClick={() => setPage(next => (next + 1))} disabled = { page === 10 | isPreviousData}>Next</button>
     </div>
   )
 }
