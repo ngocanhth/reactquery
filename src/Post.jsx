ea58752cd24d7679ec65bbb7444195a1fcb95093
ngocanh <lengocanhxd@gmail.com>
2022-10-30 17:23:34 +0700

    bai 7

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios';
-import { useQueries, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQueries, useQuery } from '@tanstack/react-query';
 import Header from './Header';
 import { useState } from 'react';
 
@@ -11,20 +11,21 @@ function Post() {
 
     const fectApi = async (context) => {
     //  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-     console.log('context:', context)
-    const response = await axios.get(`http://localhost:3001/posts/${context.queryKey[1]}`);
+   //  console.log('context:', context)
+      const response = await axios.get(`http://localhost:3001/posts/${context.queryKey[1]}`);
       // console.log(response);
       return response.data;
     }
 
-    const fectApi1 = async (context) => {
-      console.log('context:', context)
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${context.queryKey[1]}`);
+    const fectApi1 = async ({pageParam = 10}) => {
+    //  console.log('context:', context)
+     //   const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${context.queryKey[1]}`);
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${pageParam}`);
         return response.data;
     }
 
     const fectUserDetail = async (context) => {
-      console.log('context:', context)
+    //  console.log('context:', context)
       const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${context.queryKey[1]}`);
       return response.data;
   }
@@ -46,7 +47,25 @@ function Post() {
  // const { isError, isLoading, isFetching, data } = useQuery(['posts', userId ], fectUserDetail, { enabled: !!userId })
     //  console.log(data);
 
-    const { isError, isLoading, isFetching, isPreviousData, data } = useQuery(['posts', page], fectApi1, { keepPreviousData: true })
+  // bai 6 -  const { isError, isLoading, isFetching, isPreviousData, data } = useQuery(['posts', page], fectApi1, { keepPreviousData: true })
+    const { isError, isLoading, isFetching, isPreviousData, data, fetchNextPage, fetchPreviousPage, isFetchingNextPage, hasNextPage, isFetchingPreviousPage, hasPreviousPage } = useInfiniteQuery(['posts', page], fectApi1, { 
+      getNextPageParam: (lastPage, pages) => {
+  //      console.log('pages: ', pages);
+        if(pages.length <= 10) {
+          return pages.length + 1
+        }
+
+        return undefined
+      },
+      getPeviousPageParam: (lastPage, pages) => {
+        //      console.log('pages: ', pages);
+          if(pages.length <= 10) {
+            return pages.length - 1
+          }
+  
+          return undefined
+      }
+     })
 
       
       // console.log("Check loading: ", { isLoading, isFetching });
@@ -58,7 +77,7 @@ function Post() {
 
       if (isError) return <div> error </div>
 
-// console.log(data);
+      console.log("data: ", data , isFetchingNextPage, hasNextPage);
 
   return (
     <div className='posts-list'>
@@ -84,6 +103,26 @@ function Post() {
 
           <button onClick={() => setPage(prev => Math.max(1, (prev - 1)))} disabled = { page === 1 | isPreviousData}>Previous</button>
           <button onClick={() => setPage(next => (next + 1))} disabled = { page === 10 | isPreviousData}>Next</button>
+   
+          {
+            data.pages.map(page => {
+              return (
+                <h1 className='h1' key = {page.id}>{page.id} -- {page.title}</h1>
+              )
+            })
+          }
+
+          <button onClick={() => fetchNextPage({pageParam: 20})}
+          disabled={isFetchingNextPage | !hasNextPage}
+          >
+             { isFetchingNextPage ? 'Loading more' : !hasNextPage ? ' No data': '   Load More'}
+          </button>
+
+          <button onClick={() => fetchPreviousPage()}
+          disabled={isFetchingPreviousPage | !hasPreviousPage}
+          >
+             { isFetchingPreviousPage ? 'Loading more' : !hasPreviousPage ? ' No data Previous': '   Load More Previous Page'}
+          </button>
     </div>
   )
 }
